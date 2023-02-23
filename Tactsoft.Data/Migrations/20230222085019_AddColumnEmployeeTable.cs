335d0c6c33848e1934b06fe30aabfaf536740309
khpolash <polash250@gmail.com>
2023-02-23 11:57:32 +0600

    add migration for new column on employee table

---
+++ Tactsoft.Data/Migrations/20230222085019_AddColumnEmployeeTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tactsoft.Data.Migrations
+{
+    public partial class AddColumnEmployeeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdNumber",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdNumber",
+                table: "Employees");
+        }
+    }
+}
